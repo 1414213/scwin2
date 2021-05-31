cb44272ffad0db607d598a110ec6265edd4979da
1414213 <44933546+1414213@users.noreply.github.com>
2021-05-31 19:06:07 -0400

    Add files via upload

--- backend/hardwares/StickButtonCross.cs
+++ backend/hardwares/StickButtonCross.cs
@@ -13,37 +13,23 @@ namespace Backend {
 		public Button Inner { get; set; } = new ButtonKey();
 		public Button Outer { get; set; } = new ButtonKey();
 		public bool HasOverlap { get; set; } = true;
-		public double Deadzone {
-			get => this.deadzone;
-			set {
-				if (value > 1.0 || value < 0) throw new SettingNotProportionException(
-					"Deadzone must be proportion of the thumbstick's radius ([0, 1])."
-				);
-				else this.deadzone = value;
-			}
-		}
-		public double InnerRadius {
-			get => this.innerRadius;
-			set {
-				if (value > 1.0 || value < 0) throw	new SettingNotProportionException(
-					"InnerRadius must be proportion of the thumbstick's radius ([0, 1])."
-				);
-				else this.innerRadius = value;
-			}
-		}
-		public double OuterRadius {
-			get => this.outerRadius;
-			set {
-				if (value > 1.0 || value < 0) throw	new SettingNotProportionException(
-					"OuterRadius must be proportion of the thumbstick's radius ([0, 1])."
-				);
-				else this.outerRadius = value;
-			}
-		}
+		public double Deadzone { get => deadzone; set {
+			if (value > 1.0 || value < 0) throw new SettingNotProportionException(
+				"Deadzone must be proportion of the thumbstick's radius ([0, 1]).");
+			else this.deadzone = value;
+		} }
+		public double InnerRadius { get => innerRadius;	set {
+			if (value > 1.0 || value < 0) throw	new SettingNotProportionException(
+				"InnerRadius must be proportion of the thumbstick's radius ([0, 1]).");
+			else this.innerRadius = value;
+		} }
+		public double OuterRadius { get => outerRadius;	set {
+			if (value > 1.0 || value < 0) throw	new SettingNotProportionException(
+				"OuterRadius must be proportion of the thumbstick's radius ([0, 1]).");
+			else this.outerRadius = value;
+		} }
 
-		private double deadzone = 0.2;
-		private double innerRadius = 0.35;
-		private double outerRadius;
+		private double deadzone = 0.2, innerRadius = 0.35, outerRadius = 0;
 
 		public StickButtonCross() {}
 
@@ -79,8 +65,8 @@ namespace Backend {
 			double r = Math.Sqrt(coord.x * coord.x + coord.y * coord.y);
 			double theta = 0;
 			if (coord.y >= 0 && r != 0) theta = Math.Acos(coord.x / r);
-			else if (coord.y < 0) theta = -Math.Acos(coord.x / r);
-			else if (r == 0) theta = Double.NaN;
+			else if (coord.y < 0)       theta = -Math.Acos(coord.x / r);
+			else if (r == 0)            theta = Double.NaN;
 			if (Double.IsNaN(theta) || (r < Deadzone * Int16.MaxValue)) {
 				// If r is 0, then stick was reset to position 0,0.  No input occurs so all buttons are released.
 				// Also checks if the input event occured inside of the deadzone.
