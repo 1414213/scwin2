50358b51d20fe640c02bd3b7e7480eb5c6228652
1414213 <44933546+1414213@users.noreply.github.com>
2021-05-31 18:57:10 -0400

    Add files via upload

--- backend/hardwares/PadStick.cs
+++ backend/hardwares/PadStick.cs
@@ -1,53 +1,16 @@
 using System;
-using System.Collections.Generic;
-
 using api = SteamControllerApi;
-
 
 namespace Backend {
 	public class PadStick : Trackpad {
-		public double Deadzone {
-			get => this.stick.Deadzone;
-			set {
-				if (value < 0 || value > 1.0) throw new ArgumentException("Deadzone must be between 0 and 1.0");
-				this.stick.Deadzone = value;
-			}
-		}
-		public double OuterLimit {
-			get => this.outerLimit;
-			set {
-				if (value < 0 || value > 1.0) throw new ArgumentException("OuterLimit must be between 0 and 1.0");
-				this.outerLimit = value;
-			}
-		}
-		public bool IsLeftElseRight {
-			get => this.stick.IsLeftElseRight;
-			set => this.stick.IsLeftElseRight = value;
-		}
+		public double Deadzone { get => stick.Deadzone; set => stick.Deadzone = value; }
+		public double Gradingzone { get => stick.Gradingzone; set => stick.Gradingzone = value; }
+		public bool IsLeftElseRight { get => stick.IsLeftElseRight; set => stick.IsLeftElseRight = value; }
 
-		private StickStick stick = new StickStick();
-		private double outerLimit = 0.8;
+		private StickStick stick = new StickStick{ Deadzone = 0.2, Gradingzone = 0.8 };
 
 		protected override void DoEventImpl(api.InputData e) {
 			var (x, y) = e.Coordinates ?? throw new ArgumentException(e + " must be a coordinal event.");
-
-			// convert to polar coordinates
-			double r = Math.Sqrt((x * x) + (y * y));
-			double theta = 0;
-			if (y >= 0 && r != 0) theta = Math.Acos(x / r);
-			else if (y < 0) theta = -Math.Acos(x / r);
-			else if (r == 0) theta = Double.NaN;
-
-			// normalize r to an equivalent value between 0 and the outer limit
-			if (r >= outerLimit * Int16.MaxValue) {
-				r = Int16.MaxValue;
-			} else r = r / outerLimit;
-
-			// convert back into cartesian coordinates
-			if (!Double.IsNaN(theta)) {
-				x = Convert.ToInt16(r * Math.Cos(theta));
-				y = Convert.ToInt16(r * Math.Sin(theta));
-			}
 
 			// handle press or release event
 			if ((e.Flags & api.Flags.Pressed) == api.Flags.Pressed) {
@@ -55,7 +18,9 @@ namespace Backend {
 			} else if ((e.Flags & api.Flags.Released) == api.Flags.Released) {
 				stick.DoEvent(new api.InputData(api.Key.StickPush, x, y, api.Flags.AbsoluteMove));
 				stick.DoEvent(new api.InputData(api.Key.StickPush, 0, 0, api.Flags.AbsoluteMove));
-			} else throw new ArgumentException(e + " isn't a trackpad press.");
+			} else {
+				throw new ArgumentException(e + " isn't a trackpad press.");
+			}
 		}
 
 		protected override void ReleaseAllImpl() {}
--- backend/hardwares/StickStick.cs
+++ backend/hardwares/StickStick.cs
@@ -9,37 +9,60 @@ namespace Backend {
 		/// <summary>
 		/// Proportion of the thumbstick's radius.
 		/// </summary>
-		public double Deadzone {
-			get => this.deadzone;
-			set {
-				if (value < 0 || value > 1.0)
-					throw new ArgumentException("Deadzone must be proportion of the thumbstick's radius ([0, 1])");
-				this.deadzone = value;
-			}
-		}
+		public double Deadzone { get => deadzone; set {
+			if (value < 0 || value > 1.0) throw new SettingNotProportionException(
+				"Deadzone must be proportion of the thumbstick's radius [0, 1]."
+			);
+			this.deadzone = value;
+		} }
+		/// <summary>
+		/// Proportion of thumbstick's radius where degrees of input is measured, measuring from its center.
+		/// Input beyond this zone is simulated as maximum tilt.
+		/// </summary>
+		public double Gradingzone { get => gradingzone; set {
+			if (value is < 0 or > 1d) throw new SettingNotProportionException(
+				"Gradingzone must be proportion of the thumbstick's radius [0, 1].");
+			this.gradingzone = value;
+		} }
 		/// <summary>
 		/// Sends inputs to either left or right virtual thumbstick.
 		/// <summary>
 		public bool IsLeftElseRight { get; set; }
-		[JsonIgnore]
-		public override string HardwareType => "Thumbstick";
 
-		private double deadzone = 0.2;
+		double deadzone = 0.2, gradingzone = 1;
 
 		public override void DoEvent(api.InputData e) {
 			var coord = e.Coordinates ?? throw new ArgumentException(e + " must be a coordinal event.");
+			double r = Math.Sqrt(coord.x * coord.x + coord.y * coord.y);
+			double coordCos = coord.x / r;
+			double coordSin = coord.y / r;
 
 			// check if coordinate is inside the thumbstick's deadzone
-			double r = Math.Sqrt(coord.x * coord.x + coord.y * coord.y) / Int16.MaxValue;
-			if (r < deadzone) {
+			if (r < Deadzone * Int16.MaxValue || r == 0) {
 				if (IsLeftElseRight) robot.MoveLStick(0, 0);
 				else robot.MoveRStick(0, 0);
 				return;
 			}
 
+			// proportion simulated input to be inside of the grading zone
+			double r_graded = r / Gradingzone;
+
+			// var coord_graded = (x: (short)Math.Clamp(
+			//                     	(int)(r_graded * coordCos),
+			// 	                	Int16.MinValue,
+			// 	                	Int16.MaxValue
+			//                     ),
+			//                     y: (short)Math.Clamp(
+			//                     	(int)(r_graded * coordSin),
+			// 	                	Int16.MinValue,
+			// 	                	Int16.MaxValue
+			//                     ));
+			var coord_graded = (x: (short)Math.Clamp((int)(r_graded * coordCos), Int16.MinValue, Int16.MaxValue),
+			                    y: (short)Math.Clamp((int)(r_graded * coordSin), Int16.MinValue, Int16.MaxValue));
+
 			// send the steam controller's stick data to the virtual gamepad
-			if (IsLeftElseRight) robot.MoveLStick(e.Coordinates.Value.x, e.Coordinates.Value.y);
-			else robot.MoveRStick(e.Coordinates.Value.x, e.Coordinates.Value.y);
+			if (IsLeftElseRight) robot.MoveLStick(coord_graded.x, coord_graded.y);
+			else                 robot.MoveRStick(coord_graded.x, coord_graded.y);
 		}
 
 		public override void ReleaseAll() {}
