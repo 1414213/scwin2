b49b948f737c244880d78d572248aa074e55c33f
1414213 <jbehr479@gmail.com>
2021-10-16 17:08:12 -0400

    Add summaries.

--- Extensions.cs
+++ Extensions.cs
@@ -3,10 +3,9 @@ using System;
 
 namespace MethodExtensions {
 	public static class Extensions {
-
-		/* Both the start and the end are inclusive.
-		 * The array returned contains shallow copies.
-		 * */
+		/// <summary>
+		/// Both the start and the end are inclusive.  The array returned contains shallow copies.
+		/// </summary>
 		public static T[] SliceNew<T>(this T[] sourceArray, int startingIndex, int endingIndex) {
 			T[] destination = new T[(endingIndex - startingIndex) + 1];
 			for (int i = 0; i < destination.Length; i++) {
@@ -15,6 +14,7 @@ namespace MethodExtensions {
 			return destination;
 		}
 
+		/// <summary>Perform Array.Reverse(T[] array) as a member and return a reference to this array.</summary>
 		public static T[] Reverse<T>(this T[] array) {
 			Array.Reverse(array);
 			return array;
